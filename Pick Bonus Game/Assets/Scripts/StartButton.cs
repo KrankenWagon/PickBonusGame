03be2bfbce58b943654421d909fb3ea9e8e7b986
Caleb Barnhart <47796858+KrankenWagon@users.noreply.github.com>
2021-07-17 14:20:15 -0400

    Minor updates

--- Pick Bonus Game/Assets/Scripts/StartButton.cs
+++ Pick Bonus Game/Assets/Scripts/StartButton.cs
@@ -9,10 +9,14 @@ namespace Modular.Events {
 
     public class StartButton : MonoBehaviour {
         public GameEvent startRound;                                        //Game event that triggers when the player starts the round
+        public BoolVariable roundActive;                                    //Bool which holds if there is currently a round in play
 
         // This method raises the Start Round game event on mouse click
         private void OnMouseUpAsButton() {
-            startRound.Raise();
+            if (!roundActive) {
+                startRound.Raise();
+                roundActive.Value = true;
+            }
         }
     }
 }
